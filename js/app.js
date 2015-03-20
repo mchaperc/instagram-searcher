52e48776135f153775306050ff8eb0f2e48313a0
Matt Chastain <mattchastain@food.attlocal.net>
2015-03-19 20:36:20 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var myModule = angular.module('myApp', []);
+
+var InstagramSearchCtrl = myModule.controller('InstagramSearchCtrl', function($scope, $http) {
+
+	$scope.loading = false;
+	$scope.data = [];
+	$scope.images = [];
+	
+	$scope.submit = function (searchTerm) {
+		$scope.searchTerm = searchTerm;
+		$scope.loading = true;
+		$scope.goFetch(searchTerm);
+	};
+
+	$scope.goFetch = function(searchTerm) {
+		var url = "https://api.instagram.com/v1/tags/" + searchTerm + "/media/recent";
+		var request = {
+			callback: 'JSON_CALLBACK',
+			client_id: 'a5533b066261464e98aeebded5620c4c'
+		}
+		$http({
+			method: 'JSONP',
+			url: url,
+			params: request
+		})
+		.success(function(response) {
+			$scope.data = [];
+			$scope.data = response.data;
+			console.log($scope.data);
+			$scope.loading = false;
+			$scope.searchSuccess = true;
+		})
+		.error(function() {
+			alert('error');
+		})
+	}
+
+})
